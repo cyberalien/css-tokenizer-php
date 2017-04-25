8ba35dfae718cf40051464e9446e33641ab8d0a8
cyberalien <cyberalien@gmail.com>
2017-04-25 16:46:40 +0300

    Remove indexes from test results, add test for less mixins with variables

--- test/tokenizer/less_test.php
+++ test/tokenizer/less_test.php
@@ -12,22 +12,22 @@ class LessTest extends \PHPUnit\Framework\TestCase
         ]);
         $result = $parser->tokenize('.box-shadow() { color: red; }');
         $this->assertEquals(0, count($parser->errors));
+        foreach ($result as &$item) {
+            unset ($item['index']);
+        }
         $this->assertEquals([
             [
                 'token' => '{',
                 'code'  => '.box-shadow()',
                 'selectors' => ['.box-shadow()'],
-                'index' => 0,
             ],
             [
                 'token' => 'rule',
                 'key'  => 'color',
                 'value' => 'red',
-                'index' => 15,
             ],
             [
                 'token' => '}',
-                'index' => 28,
             ]
         ], $result);
     }
@@ -40,34 +40,32 @@ class LessTest extends \PHPUnit\Framework\TestCase
         ]);
         $result = $parser->tokenize('$test: black; $foo: lighten(#400, 20%); a { color: $test; }');
         $this->assertEquals(0, count($parser->errors));
+        foreach ($result as &$item) {
+            unset ($item['index']);
+        }
         $this->assertEquals([
             [
                 'token' => 'rule',
                 'key'  => '$test',
                 'value' => 'black',
-                'index' => 0,
             ],
             [
                 'token' => 'rule',
                 'key'  => '$foo',
                 'value' => 'lighten(#400, 20%)',
-                'index' => 13,
             ],
             [
                 'token' => '{',
                 'code'  => 'a',
                 'selectors' => ['a'],
-                'index' => 39,
             ],
             [
                 'token' => 'rule',
                 'key'  => 'color',
                 'value' => '$test',
-                'index' => 43,
             ],
             [
                 'token' => '}',
-                'index' => 58,
             ]
         ], $result);
 
@@ -88,65 +86,144 @@ class LessTest extends \PHPUnit\Framework\TestCase
   .mixin(lighten(#efca44, 10%); @padding: 40px);
 }');
         $this->assertEquals(0, count($parser->errors));
+        foreach ($result as &$item) {
+            unset ($item['index']);
+        }
         $this->assertEquals([
             [
                 'token' => '{',
                 'code'  => '.mixin(@color: black; @margin: 10px; @padding: 20px)',
                 'selectors' => ['.mixin(@color: black; @margin: 10px; @padding: 20px)'],
-                'index' => 0,
             ],
             [
                 'token' => 'rule',
                 'key'  => 'color',
                 'value' => '@color',
-                'index' => 54,
             ],
             [
                 'token' => 'rule',
                 'key'  => 'margin',
                 'value' => '@margin',
-                'index' => 71,
             ],
             [
                 'token' => 'rule',
                 'key'  => 'padding',
                 'value' => '@padding',
-                'index' => 90,
             ],
             [
                 'token' => '}',
-                'index' => 112,
             ],
             [
                 'token' => '{',
                 'code'  => '.class1',
                 'selectors' => ['.class1'],
-                'index' => 113,
             ],
             [
                 'token' => 'code',
                 'code'  => '.mixin(@margin: 20px; @color: #33acfe);',
-                'index' => 123,
             ],
             [
                 'token' => '}',
-                'index' => 166,
             ],
             [
                 'token' => '{',
                 'code'  => '.class2',
                 'selectors' => ['.class2'],
-                'index' => 167,
             ],
             [
                 'token' => 'code',
                 'code'  => '.mixin(lighten(#efca44, 10%); @padding: 40px);',
-                'index' => 177,
             ],
             [
                 'token' => '}',
-                'index' => 227,
             ]
+        ], $result);
+    }
+
+    public function testMixinWithVariables()
+    {
+        $parser = new Tokenizer([
+            'ignoreErrors'  => false,
+            'lessSyntax'    => true
+        ]);
+        $result = $parser->tokenize('.box-shadow(@style, @c) when (iscolor(@c)) {
+  -webkit-box-shadow: @style @c;
+  box-shadow:         @style @c;
+}
+.box-shadow(@style, @alpha: 50%) when (isnumber(@alpha)) {
+  .box-shadow(@style, rgba(0, 0, 0, @alpha));
+}
+.box {
+  color: saturate(@base, 5%);
+  border-color: lighten(@base, 30%);
+  div { .box-shadow(0 0 5px, 30%) }
+}
+');
+        $this->assertEquals(0, count($parser->errors));
+        foreach ($result as &$item) {
+            unset ($item['index']);
+        }
+        $this->assertEquals([
+            [
+                'token' => '{',
+                'code'  => '.box-shadow(@style, @c) when (iscolor(@c))',
+                'selectors' => ['.box-shadow(@style, @c) when (iscolor(@c))'],
+            ],
+            [
+                'token' => 'rule',
+                'key'  => '-webkit-box-shadow',
+                'value' => '@style @c',
+            ],
+            [
+                'token' => 'rule',
+                'key'  => 'box-shadow',
+                'value' => '@style @c',
+            ],
+            [
+                'token' => '}',
+            ],
+            [
+                'token' => '{',
+                'code'  => '.box-shadow(@style, @alpha: 50%) when (isnumber(@alpha))',
+                'selectors' => ['.box-shadow(@style, @alpha: 50%) when (isnumber(@alpha))'],
+            ],
+            [
+                'token' => 'code',
+                'code'  => '.box-shadow(@style, rgba(0, 0, 0, @alpha));',
+            ],
+            [
+                'token' => '}',
+            ],
+            [
+                'token' => '{',
+                'code'  => '.box',
+                'selectors' => ['.box'],
+            ],
+            [
+                'token' => 'rule',
+                'key'  => 'color',
+                'value' => 'saturate(@base, 5%)',
+            ],
+            [
+                'token' => 'rule',
+                'key'  => 'border-color',
+                'value' => 'lighten(@base, 30%)',
+            ],
+            [
+                'token' => '{',
+                'code'  => 'div',
+                'selectors' => ['div'],
+            ],
+            [
+                'token' => 'code',
+                'code'  => '.box-shadow(0 0 5px, 30%)',
+            ],
+            [
+                'token' => '}',
+            ],
+            [
+                'token' => '}',
+            ],
         ], $result);
     }
 }
